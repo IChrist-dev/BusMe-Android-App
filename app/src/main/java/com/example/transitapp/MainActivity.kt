e1a1079c236407d32fa1db5d74613d963eccced5
Ian Christian <w0480449@nscc.ca>
2023-11-08 16:05:13 -0400

    New project. Retrieves GPS coordinates after permission request.

---
+++ app/src/main/java/com/example/transitapp/MainActivity.kt
@@ -0,0 +1,41 @@
+package com.example.transitapp
+
+import android.os.Bundle
+import android.util.Log
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.example.transitapp.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // Get location from Start Activity Intent
+        val intent = intent
+        val latitude: Double = intent.getDoubleExtra("latitude", 0.0)
+        val longitude: Double = intent.getDoubleExtra("longitude", 0.0)
+
+        Log.i("TESTING", "Latitude: $latitude\nLongitude: $longitude")
+
+        // Bottom Navigation View setup
+        val navView: BottomNavigationView = binding.navView
+
+        val navController = findNavController(R.id.nav_host_fragment_activity_main)
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        val appBarConfiguration = AppBarConfiguration(setOf(
+                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications))
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        navView.setupWithNavController(navController)
+    }
+}
